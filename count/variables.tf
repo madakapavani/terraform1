4395c5194eb825f3fd23abb098b957cdcaaf64a3
pavani <pvr9602@gmail.com>
2025-03-23 10:37:20 +0530

    terraform

---
+++ count/variables.tf
@@ -0,0 +1,13 @@
+variable "instances" {
+  default = ["mysql", "backend", "frontend"]
+}
+
+
+variable "zone_id" {
+  default = "Z04026461RROCAMAICOCN"
+}
+
+variable "domain_name" {
+  default = "ulasala.online"
+}
+
